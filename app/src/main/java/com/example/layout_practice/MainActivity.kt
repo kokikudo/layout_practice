e656cf3fc16aaeca05d07560caf7a156ff195abf
kokikudo <massanmesu@icloud.com>
2021-12-27 16:22:43 +0900

    やる気出たら書く。本書P235

--- app/src/main/java/com/example/layout_practice/MainActivity.kt
+++ app/src/main/java/com/example/layout_practice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.layout_practice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// やる気出たらコード書く
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
